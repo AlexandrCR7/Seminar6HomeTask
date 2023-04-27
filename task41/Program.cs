6ec9c99d994ff13d65ab049b557c84d28b892ebd
Alexandr <k9108@mail.ru>
2023-04-27 17:52:44 +0300

    Add HomeTask

---
+++ task41/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int InputNumber(string numbers)
+{
+    System.Console.Write($"Введите {numbers}");
+    int i;
+    while(!int.TryParse(System.Console.ReadLine(), out i))
+    {
+        System.Console.WriteLine("Это не число, повторите ввод");
+    }
+    return i;
+}
+
+int MinusNumber(int argument)
+{
+    int x = argument;
+    if(x < 0)
+    {
+        System.Console.WriteLine("Размер массива не может быть меньше 0");
+    }
+    
+    return argument;
+}
+
+int[] FillArray(int numbers)
+{
+    int[] Array = new int[numbers];
+    for(int i = 0; i < numbers; i++)
+    {
+        Array[i] = InputNumber("значение ");
+    }
+    return Array;
+}
+
+
+void PrintArray (int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        System.Console.Write($"{numbers[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+int CountNumbers (int[] numbers)
+{
+    int summ = 0;
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        if(numbers[i] > 0)
+        {
+            summ++;
+        }
+    }
+    return summ;
+}
+
+void Print()
+{
+    int Massive = InputNumber("размер массива ");
+    MinusNumber(Massive);
+    int[] Mas = FillArray(Massive);
+    PrintArray(Mas);
+    CountNumbers(Mas);
+    System.Console.WriteLine($"Кол-во положительных чисел = {CountNumbers(Mas)}");
+}
+
+Print();
---
+++ task43/Program.cs
@@ -0,0 +1,60 @@
+﻿
+int numberB1 ()
+{
+    System.Console.Write("Введите b1 = ");
+    int i;
+    while(!int.TryParse(System.Console.ReadLine(), out i))
+    {
+        System.Console.WriteLine("Это не число, повторите ввод");
+    }
+    return i;
+}
+
+int numberB2 ()
+{
+    System.Console.Write("Введите b2 = ");
+    int i;
+    while(!int.TryParse(System.Console.ReadLine(), out i))
+    {
+        System.Console.WriteLine("Это не число, повторите ввод");
+    }
+    return i;
+}
+
+int numberK1 ()
+{
+    System.Console.Write("Введите k1 = ");
+    int i;
+    while(!int.TryParse(System.Console.ReadLine(), out i))
+    {
+        System.Console.WriteLine("Это не число, повторите ввод");
+    }
+    return i;
+}
+
+int numberK2 ()
+{
+    System.Console.Write("Введите k2 = ");
+    int i;
+    while(!int.TryParse(System.Console.ReadLine(), out i))
+    {
+        System.Console.WriteLine("Это не число, повторите ввод");
+    }
+    return i;
+}
+
+void numberX ()
+{
+    int b2 = numberB2();
+    int b1 = numberB1();
+    int k1 = numberK1();
+    int k2 = numberK2();
+    int x = (b2 - b1)/(k1 - k2);
+    System.Console.WriteLine($"x = {x}");
+    int y = k1 * x + b1;
+    System.Console.WriteLine($"y = {y}");
+    int y1 = y = k2 * x + b2;
+    System.Console.WriteLine($"y1 = {y1}");
+}
+
+numberX();
